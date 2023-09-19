6b4a98630d12b32c00042f157e0f7b370384ea28
William Sylvestre <farakwarts@gmail.com>
2023-09-19 10:06:39 -0400

    Ajout outil météo

---
+++ MonOutil/Meteo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonOutil
+{
+    internal class Meteo
+    {
+        public Meteo()
+        {
+            string? tempCelsius;
+            double tempFahrenheit;
+            double convert;
+
+            Console.WriteLine("Bivenue à l'outil Météo.");
+            Console.Write("Quelle est la température actuelle en celsius?: ");
+            tempCelsius = Console.ReadLine();
+            convert= Convert.ToDouble(tempCelsius);
+            tempFahrenheit = (convert * 9 / 5) + 32;
+
+            Console.WriteLine($"Il fait présentement {tempFahrenheit}°F.");
+        }
+    }
+}
--- MonOutil/Program.cs
+++ MonOutil/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//-1- Variable
+using MonOutil;
+
+string? userName;
+
+//-2- Code
+Console.Write("Bonjour, quel est votre nom?: ");
+userName = Console.ReadLine();
+
+//Alternatif à Console.WriteLine("Bienvenue " + userName + " à MonOutil.");
+Console.WriteLine($"Bienvenue {userName} à MonOutil.");
+
+new Meteo();
