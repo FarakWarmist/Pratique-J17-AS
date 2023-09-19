44a64a9004fff09ad2da17c6b588cad9f00637f1
William Sylvestre <farakwarts@gmail.com>
2023-09-19 13:23:26 -0400

    Correction outil météo

--- MonOutil/Meteo.cs
+++ MonOutil/Meteo.cs
@@ -10,17 +10,44 @@ namespace MonOutil
     {
         public Meteo()
         {
-            string? tempCelsius;
-            double tempFahrenheit;
+            string? tempA;
+            double tempB;
             double convert;
+            string? tempType;
+            string? tempSymbol;
 
             Console.WriteLine("Bivenue à l'outil Météo.");
-            Console.Write("Quelle est la température actuelle en celsius?: ");
-            tempCelsius = Console.ReadLine();
-            convert= Convert.ToDouble(tempCelsius);
-            tempFahrenheit = (convert * 9 / 5) + 32;
+            Console.WriteLine("Voulez-vous convertir la température en Celsius (C) ou en Fahrenheit (F)? : \n(C/F)");
 
-            Console.WriteLine($"Il fait présentement {tempFahrenheit}°F.");
+            while (true)
+            {
+                tempType = Console.ReadLine();
+                if (tempType == "f" || tempType == "F")
+                {
+                    Console.Write("Quelle est la température actuelle en celsius?: ");
+                    tempA = Console.ReadLine();
+                    convert = Convert.ToDouble(tempA);
+                    tempB = (convert * 9 / 5) + 32;
+                    tempSymbol = "°F";
+                    break;
+                }
+                else if (tempType == "c" || tempType == "C")
+                {
+                    Console.Write("Quelle est la température actuelle en fahrenheit?: ");
+                    tempA = Console.ReadLine();
+                    convert = Convert.ToDouble(tempA);
+                    tempB = Math.Round((convert - 32) * 5 / 9, 2);
+                    tempSymbol = "°C";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Veillez entrer C ou F");
+
+                }
+            }
+
+            Console.WriteLine($"Il fait présentement {tempB}{tempSymbol}.");
         }
     }
 }
